5dd1bafb1c53a5a1b18f50ace0354452ef2aab6b
Zebnitsky Max <Zebnitsky.Max87@gmail.com>
2023-12-19 01:02:57 +0300

    feat:finaly done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -609,13 +609,14 @@ return rot13(str)
  *   'K♠' => 51
  */
 
-function getCardId(/* value */) {
+function getCardId(value) {
   // throw new Error('Not implemented');
-  // eslint-disable-next-line prefer-const
+
   let arr = ['A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
   'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
   'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
   'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠']
+ return arr.findIndex(elem => elem === value)
 }
 
 module.exports = {
